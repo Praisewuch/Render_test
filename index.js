4976bed957f4f5128a1a110e641b98258bf2c0be
Praise W <praisewuche69@gmail.com>
2025-01-17 15:18:16 +0100

    finsihed intro bug fixes

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
 require('dotenv').config()
-const Note = require('./models/note');
-const note = require('./models/note');
+const Note = require('./models/note')
 
-
-app.use(cors());
-app.use(morgan('tiny'));
-app.use(express.static('dist'));
+app.use(cors())
+app.use(morgan('tiny'))
+app.use(express.static('dist'))
 app.use(express.json())
 
 
 
 
 app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-      response.json(notes)
-    })
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
-   Note.findById(request.params.id).then(note => {
-      if(note){
-        response.json(note)
-      } else{
-        response.status(404).end()
-      }
-   }) 
-   .catch(err => {error => next(error)})
+  Note.findById(request.params.id).then(note => {
+    if(note){
+      response.json(note)
+    } else{
+      response.status(404).end()
+    }
+  })
+    .catch(() => {error => next(error)})
 })
 
-app.delete('/api/notes/:id',(request,response) => {
-  const id = request.params.id;
-  Note.findByIdAndDelete(id).then(result => {
+app.delete('/api/notes/:id',(request,response,next) => {
+  const id = request.params.id
+  Note.findByIdAndDelete(id).then(() => {
     response.status(204).end()
   })
-  .catch(err => next(err))
+    .catch(err => next(err))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const {content, important}= request.body
+  const { content, important }= request.body
 
-  Note.findByIdAndUpdate(request.params.id, {content, important}, {new: true,runValidators: true, context:'query'}).then(updateNote => {
+  Note.findByIdAndUpdate(request.params.id, { content, important }, { new: true,runValidators: true, context:'query' }).then(updateNote => {
     response.json(updateNote)
   }).catch(err => next(err))
 })
@@ -61,9 +59,9 @@ app.post('/api/notes', (request, response,next) => {
     content: body.content,
     important: body.important || false,
   })
-  
+
   note.save().then(savedNote => {
-    response.status(201).json(savedNote);
+    response.status(201).json(savedNote)
   }).catch(err => next(err))
 })
 
@@ -76,19 +74,19 @@ app.use(unknownEndpoint)
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
-  if(error.name === "CastError"){
-    return response.status(400).send({error: 'malformatted id'})
-  }else if(error.name === "ValidationError"){
-    return response.status(400).json({error: error.message})
+  if(error.name === 'CastError'){
+    return response.status(400).send({ error: 'malformatted id' })
+  }else if(error.name === 'ValidationError'){
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
 }
 app.use(errorHandler)
 
- //search functionlaity of json-parser
+//search functionlaity of json-parser
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`)
+  console.log(`Server running on port http://localhost:${PORT}`)
 })
